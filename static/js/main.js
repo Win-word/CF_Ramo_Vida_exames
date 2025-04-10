4de9a11a441b4c1fb3a014b1986ec2dc7f46fec0
Win_Score <ws25jan19@gmail.com>
2025-04-11 04:43:28 +1100

    ancajamento do js

--- static/js/main.js
+++ static/js/main.js
@@ -7,14 +7,18 @@ alert("Pagina de Realizacao de exame!");
 
 
 function changePage1(){
-    
+    alert("pagina 1");
     cp.src = "/static/exame/imageExame1.jpg";
     b1.style.background = "#2474c3";
     b2.style.background = "white";
 
+
+
+
 }
 
 function changePage2(){
+    alert("pagina 2");
     
     cp.src = "/static/exame/imageExame2.jpg";
     b2.style.background = "#2474c3";
